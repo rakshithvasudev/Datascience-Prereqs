8fff18acaef3bbc6d4ba150604f0a0f1f5a4187f
Rakshith Vasudev <rakshithvbharani@gmail.com>
2017-06-06 10:46:56 -0700

    added pandas playground

--- Pandas/pandasplayground.py
+++ Pandas/pandasplayground.py
@@ -1,3 +1,9 @@
-import pandas
+import numpy as np
+import pandas as pd
 
-dict = {}
+data = {'Company':['GOOGLE','GOOGLE','MSFT','MSFT','FACEBOOK','FACEBOOK'],
+'User':['Sam','Mikie','Amy','Vanessa','Malburg','Richo'],
+'Sales':[200,300,150,110,100,100]}
+
+df = pd.DataFrame(data)
+print(df)
